b58234fbcd39d784acee9ef5cc7d8c17aae735f2
anik3tra0 <anik3tra0@gmail.com>
2017-01-07 13:44:47 +0530

    Folder Rename

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -28,6 +28,7 @@ class FoldersController < ApplicationController
 
   # GET /folders/1/edit
   def edit
+    @current_folder = @folder.parent
   end
 
   # POST /folders
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'browse/:folder_id' => 'home#browse', :as => 'browse'
   get 'browse/:folder_id/new_folder' => 'folders#new', :as => 'new_sub_folder'
   get 'browse/:folder_id/new_file' => 'uploads#new', :as => 'new_sub_file'
+  get 'browse/:folder_id/rename' => 'folders#edit', :as => 'rename_folder'
   mount FileUploader::UploadEndpoint => '/files/upload'
   resources :uploads
   devise_for :users
